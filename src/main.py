b248c0fcf2ab9328dbd5d2d5d3e0c29ec70ca4b1
vohrr <matthew.d.pingel@gmail.com>
2025-02-20 17:10:59 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    node = TextNode("my new node", TextType.NORMAL, "https://google.com")
+    print(node)
+
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGES = "images"
+
+class TextNode():
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, node):
+        return self.text == node.text and self.text_type == node.text_type and self.url == node.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})" 
+           
+
